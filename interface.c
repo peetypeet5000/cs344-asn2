a6c4abb9a17a45873500eaf7fcb1deb720982e39
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-10-18 23:33:09 -0700

    bug fixes, improved comments

--- interface.c
+++ interface.c
@@ -11,6 +11,7 @@ void file_menu() {
     DIR* working_dir =  opendir(".");
     struct dirent *aDir;
 
+    // Infitintly loop menu until a valid selection is made
     while(good == false) {
         printf("\nWhich file you want to process?\nEnter 1 to pick the largest file\nEnter 2 to pick the smallest file\nEnter 3 to specify the name of a file\n\n");
         printf("Enter a choice from 1 to 3: ");
@@ -29,23 +30,29 @@ void file_menu() {
             case 3:
                 file = get_named_file(working_dir, &good);
                 break;
-
             // Invalid Choice
             default:
                 printf("\nYou entered an incorrect choice. Try again.\n");
                 good = false;
                 break;
         }
+
+        // Rewind dir for the next loop if nessesary
+        rewinddir(working_dir);
     }
 
-    //If file sucessfully found, process it
+    // If file sucessfully found, process it
     printf("\nNow processing the chosen file named %s\n", file);
     process_file(file);
 
+    // Close the dir we opened
     closedir(working_dir);
 }
 
 
+// Loops thru given dir and finds largest file
+// using the stat() function
+// good is passed by ref to record if it was sucessful
 char* get_largest_file(DIR* dir, bool* good) {
     struct dirent* file;
     struct stat file_stat;
@@ -79,7 +86,8 @@ char* get_largest_file(DIR* dir, bool* good) {
 }
 
 
-
+// Loops thru given dir and finds smallest file
+// using the stat() function
 char* get_smallest_file(DIR* dir, bool* good) {
     struct dirent* file;
     struct stat file_stat;
@@ -106,6 +114,7 @@ char* get_smallest_file(DIR* dir, bool* good) {
         *good = true;
         return smallest_name;
     } else {
+        // If not found, print error and return nothing
         printf("No matching files found\n");
         *good = false;
         return "";
@@ -114,6 +123,8 @@ char* get_smallest_file(DIR* dir, bool* good) {
 
 
 
+// Accepts user inputs then loops thru
+// given dir to check if file exists. If so, returns name
 char* get_named_file(DIR* dir, bool* good) {
     char user_input[255];
     struct dirent* file;
@@ -124,8 +135,8 @@ char* get_named_file(DIR* dir, bool* good) {
 
     // Read each file in directory
     while((file = readdir(dir)) != NULL){
-        // If the first part of the name matches the prefix, check it
-        if((strncmp(file->d_name, user_input, strlen(user_input)) == 0)) {
+        // If the file in the directory matches the user input, return it
+        if((strncmp(file->d_name, user_input, strlen(file->d_name)) == 0) && strlen(user_input) == strlen(file->d_name)) {
             *good = true;
             return file->d_name;
         }
@@ -141,15 +152,18 @@ char* get_named_file(DIR* dir, bool* good) {
 
 
 
+// Checks that the extension on a file
+// name is .csv
 bool is_csv(char* file) {
+    // Find the last occurance of the . in a name
     char* file_end = strrchr(file, '.');
    
+   // If it's not at the end of the file, check the last 4 characters are .csv
    if(file_end != NULL) {
         if(strncmp(file_end, ".csv", 4) == 0) {
             return true;
         }
    }
-
 
     return false;
 }
